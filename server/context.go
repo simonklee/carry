14ac0ea87616b48204037cada680e11834044cf2
Simon Zimmermann <simon@insmo.com>
2014-07-14 11:03:04 +0200

    server: rm unused func

--- server/context.go
+++ server/context.go
@@ -6,11 +6,9 @@ package server
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/simonz05/carry"
 	"github.com/simonz05/carry/config"
-	"github.com/simonz05/util/httputil"
 )
 
 type context struct {
@@ -37,16 +35,4 @@ func (c *context) Close() error {
 		return closer.Close()
 	}
 	return nil
-}
-
-func (c *context) handlerFunc(f func(w http.ResponseWriter, r *http.Request) (interface{}, error)) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		data, err := f(w, r)
-
-		if err != nil {
-			httputil.ServeJSONError(w, err)
-		} else {
-			httputil.ReturnJSON(w, data)
-		}
-	})
 }
